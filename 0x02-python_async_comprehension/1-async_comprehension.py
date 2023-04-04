f7e65fea1e499488d044b02dc6c031981468c607
safarilewis <lleewwii1@gmail.com>
2023-04-05 00:58:43 +0300

    async bugs still

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,9 +3,8 @@
 from typing import List
 from importlib import import_module as using
 
-# async_generator = 0-async_generator.async_generator
-async_generator = using('0-async_generator').async_generator
 
+async_generator = using('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     '''Creating a list of 10 numbers'''
