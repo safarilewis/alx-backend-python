244bf07ff8febd963b90b616008a825e8532e69b
safarilewis <lleewwii1@gmail.com>
2023-04-05 01:01:55 +0300

    bugs are a nightmare

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
-'''Executing async in parallel'''
-
+'''Task 2's module.
+'''
 import asyncio
 import time
 from importlib import import_module as using
 
-async_comprehension = using(1-async_comprehension).async_comprehension
+
+async_comprehension = using('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    '''function that executes async'''
-    start = time.time()
+    '''Executes async_comprehension 4 times and measures the
+    total execution time.
+    '''
+    start_time = time.time()
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
-    return time.time() - start
+    return time.time() - start_time
