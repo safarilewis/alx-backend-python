f4a99497591c74c952a2aa5b9ee48f387a750852
safarilewis <lleewwii1@gmail.com>
2023-04-07 15:56:12 +0300

    task upload

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''Takes two strings and returns a concatenated screen'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''Concatenates two strings'''
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Flooring ints and type annotations'''
+import math
+
+
+def floor(n: int) -> int:
+    '''Returns a floored int'''
+    return math.floor(n)
