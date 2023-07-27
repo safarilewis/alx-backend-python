fd9df37382f39b5aa9539ddc6ade2a2a3d288f8d
safarilewis <lleewwii1@gmail.com>
2023-07-27 13:39:37 +0300

    fixing line too long error

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -5,7 +5,8 @@ from typing import TypeVar, Any, Mapping, Union
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
     '''Gets value from a list using key
     '''
     if key in dct:
