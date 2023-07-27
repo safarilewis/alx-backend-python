7b02523cd32c27d095afe9abe00cb5a5d9eeb849
safarilewis <lleewwii1@gmail.com>
2023-07-27 13:26:38 +0300

    making the return type a sequence of ints

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,7 +4,7 @@
 from typing import List, Iterable, Sequence, Tuple
 
 
-def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence[int]]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     '''Computes the length of a List
     '''
     return [(i, len(i)) for i in lst]
