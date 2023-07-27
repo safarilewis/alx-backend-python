c837d0c04b420b36a5de6c238e14804c67b18d1e
safarilewis <lleewwii1@gmail.com>
2023-07-27 13:24:35 +0300

    iterable

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Let's duck type an iterable object
+'''
+from typing import List, Iterable, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence]]:
+    '''Computes the length of a List
+    '''
+    return [(i, len(i)) for i in lst]
